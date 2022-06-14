ac93f542ce77d99276e18a3fb1d21ece70df4313
PISIT KOOLPLUKPOL <pisit.koolplukpol@outlook.com>
2022-06-14 22:36:51 +0700

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Pisit Koolplukpol on 14/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
